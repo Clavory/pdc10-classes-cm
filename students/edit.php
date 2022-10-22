5b2cda3fff9180df4c6579dc86806d9657023b34
Clavory <allapitan.nathaniel@auf.edu.ph>
2022-10-22 23:15:11 +0800

    fifth commit

--- students/edit.php
+++ students/edit.php
@@ -3,23 +3,23 @@
 include ("../init.php");
 use Models\Student;
 
-    $id = $_GET['student_id'];
+    $id = $_GET['id'];
 
     $student = new Student('','','','','','');
     $student->setConnection($connection);
-    $student->getById($student_id);
-    $first_name = $student->getFirstName();
-    $last_name = $student->getLastName();
+    $student->getStudentId($id);
+    $student_first_name = $student->getFirstName();
+    $student_last_name = $student->getLastName();
     $student_number = $student->getStudentNumber();
-    $email = $student->getEmail();
-    $contact = $student->getContact();
-    $program = $student->getProgram();
+    $student_email = $student->getEmail();
+    $student_contact = $student->getContactNumber();
+    $student_program = $student->getProgram();
 
     // $all_students = $student->showAllStudents();
 
     // echo $mustache->render('student/edit', compact('student'));
 
     $template = $mustache->loadTemplate('students/edit.mustache');
-    echo $template->render(compact('id', 'student', 'first_name', 'last_name', 'student_number', 'email', 'contact', 'program'));
+    echo $template->render(compact('id', 'student', 'student_first_name', 'student_last_name', 'student_number', 'student_email', 'student_contact', 'student_program'));
 
 ?>
